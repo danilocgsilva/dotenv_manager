43874dc9c5309f805563edcd1339ee7cc70b7edc
Danilo Silva <contact@danilocgsilva.me>
2021-04-01 09:36:00 -0300

    Starting tests for filesystem

--- tests/test_SQLiteData.py
+++ tests/test_SQLiteData.py
@@ -2,6 +2,8 @@ import unittest
 from dotenv_manager.SQLiteData import SQLiteData
 from pathlib import Path
 import os
+from danilocgsilvame_python_helpers.DcgsPythonHelpers import DcgsPythonHelpers
+import tempfile
 
 class test_SQLiteData(unittest.TestCase):
 
@@ -22,6 +24,10 @@ class test_SQLiteData(unittest.TestCase):
         existsGroup = sqliteData.environmentGroup(environmentGroupName).exists()
         self.assertTrue(existsGroup)
 
+    def test_return_string_creating_empty_database(self):
+        temporaryDir = self.__makeTemporaryTestDir()
+        return self.assertFalse(True)
+
     def test_correct_path_for_sqlite(self):
 
         sqliteData = SQLiteData()
@@ -31,3 +37,14 @@ class test_SQLiteData(unittest.TestCase):
         expected_path = os.path.join(str(Path.home()), databaseName)
         object_path = sqliteData.getDatabasePath()
         self.assertEqual(expected_path, object_path)
+
+    def __getTemporaryTestDir(self):
+        dateHash = DcgsPythonHelpers().getHashDateFromDate()
+        systemTemporaryLocation = tempfile.gettempdir()
+        return os.path.join(systemTemporaryLocation, "dotenv_manager_unit_test-" + dateHash)
+
+    def __makeTemporaryTestDir(self):
+        temporaryDir = self.__getTemporaryTestDir()
+        os.makedirs(temporaryDir)
+        return temporaryDir
+        
